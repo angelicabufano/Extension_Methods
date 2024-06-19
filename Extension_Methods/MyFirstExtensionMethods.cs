0c491f2f33a27df5c2fcba3ac91afe0759aff085
Angelica Bufano <abufano@tql.com>
2024-06-19 15:19:06 -0400

    Add project files.

---
+++ Extension_Methods/MyFirstExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Methods;
+public static class MyFirstExtensionMethods {                  // change "internal" to "public" , make class static by putting "static" infront of class
+
+    public static void Debug(this string str, string label) {    // put void so that it doesnt have to return anything , always put "this" then type, then name
+        Console.WriteLine($"{label}: [{str}]");
+       
+    }
+
+    public static void Debug(this int i, string label) {
+        Console.WriteLine($"{label}: [{i}]");
+    }
+    
+    public static int Cubed(this int i) {
+        return i * i * i;
+    }
+
+    public static int Funct(this int x) {
+        return (x * x) + (x * 3) + 5;
+    }
+}
---
+++ Extension_Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Extension_Methods;
+
+internal class Program {
+    static void Main(string[] args) {
+
+        var str = "This is a test.";
+
+        str.Debug("str is");
+
+        var i = 12;
+        i.Debug("i is ");
+
+        i.Cubed().Debug("i cubed");
+
+        i.Funct().Debug($"f({i}) = ");
+
+    }
+}
